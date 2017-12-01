c02d637a7fa10acb480b6eed3427b14f95053bbd
Gergő Móricz <mo.geryy@gmail.com>
2017-12-01 17:06:39 +0100

    Upgraded from older RGBDS and XLINK

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-rgbasm -omain.obj main.asm
-xlink -mmain.map -nmain.sym LINK
+rgbasm -o main.obj main.asm
+rgblink -o robofactory.gbc main.obj
 rgbfix -p 0 -v robofactory.gbc
 bgb robofactory.gbc
